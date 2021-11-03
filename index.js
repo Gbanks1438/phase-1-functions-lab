70f02ddc09d8748cd80915ffc76a3e09299403b7
Gregory Banks <gbanks1438@gmail.com>
2021-11-02 22:14:38 -0400

    commit tree b382a46069753bad6470d306a4649c517d9a0469

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = (from, base=42) => from > base ? from - base : base - from
+const distanceFromHqInFeet = block => {
+    let distance = distanceFromHqInBlocks(block);
+    return (distance * 264);
+}
+const distanceTravelledInFeet = (spot1, spot2) => {
+    let feet = Math.abs(spot1 - spot2)
+    return (feet * 264)
+}
+const calculatesFarePrice = (spot1, spot2) => {
+let fare = distanceTravelledInFeet(spot1, spot2);
+if (fare <= 400) {
+    return 0;
+} else if (fare <= 2000) {
+    return (fare - 400) * 0.02;
+} else if (fare <= 2500) {
+    return 25;
+} else {
+    return 'cannot travel that far';
+}
+}
